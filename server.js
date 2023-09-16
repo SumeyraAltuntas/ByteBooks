49768a4001bf293974211e266711f22dc9f85085
AngieCana <grilozangie@gmail.com>
2023-09-16 14:25:11 -0400

    connected to mongodb

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const axios = require('axios')
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config('/env')
     console.log(process.env) // remove this after you've confirmed it is working
 
 const app = express();
 
 //Connect mongoose when we have mongoose set up
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB.");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 
 //define port that will be responding from the backend. if the first port does not work, it will default to 3001
